4a2cf393001b661608d4063a71f44e902b31b28f
Sergey Petrov <90527574+ptrvsrg@users.noreply.github.com>
2022-09-17 12:16:03 +0700

    Delete Task1 directory

--- Task1/README.md
+++
@@ -1,1 +0,0 @@
-[Условие задачи](https://docs.google.com/document/d/1MVgh5bx3bmYE7L91awPPuQ5ig5Qpku1_TYkBlQif25w/edit)
--- Task1/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "big_int.h"
-
-int main()
-{
-    BigInt number("255");
-    std::cout << number << std::endl;
-    std::cout << ~number << std::endl;
-}
