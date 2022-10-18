be1f9528cb1ba26b26aa804e6db1b01ab3760300
ptrvsrg <s.petrov1@g.nsu.ru>
2022-10-18 23:34:29 +0700

    Add tit for tat strategy library tests

--- Task2/libs/strategies/non_trivial/tit_for_tat/test/tests_tit_for_tat.cpp
+++ Task2/libs/strategies/non_trivial/tit_for_tat/test/tests_tit_for_tat.cpp
@@ -1,12 +1,39 @@
 #include "gtest/gtest.h"
 #include "tit_for_tat.h"
 
-#include <array>
-
-struct MoveArg
+struct ChoiceArgs
 {
-    Choice opponent;
+    Choice opponent1_choice_;
+    Choice opponent2_choice_;
+    Choice strategy_choice_;
+
+    ChoiceArgs(Choice opponent1_choice,
+               Choice opponent2_choice,
+               Choice strategy_choice)
+    :   opponent1_choice_(opponent1_choice),
+        opponent2_choice_(opponent2_choice),
+        strategy_choice_(strategy_choice) {}
 };
+
+TEST(test_tit_for_tat, test_vote_update)
+{
+    std::vector<ChoiceArgs> steps =
+    {
+        ChoiceArgs(D, C, C),
+        ChoiceArgs(C, D, D),
+        ChoiceArgs(D, D, D),
+        ChoiceArgs(C, C, D),
+        ChoiceArgs(C, C, C)
+    };
+
+    TitForTatStrategy strategy;
+
+    for (ChoiceArgs step : steps)
+    {
+        EXPECT_EQ(strategy.vote(), step.strategy_choice_);
+        strategy.update(step.opponent1_choice_, step.opponent2_choice_);
+    }
+}
 
 int main(int argc, char** argv)
 {
