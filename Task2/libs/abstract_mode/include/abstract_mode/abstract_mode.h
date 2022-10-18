1520610267dfdfbbad25558da09b7e68783782a5
ptrvsrg <s.petrov1@g.nsu.ru>
2022-10-18 23:29:40 +0700

    Remove loaded strategy class

--- Task2/libs/abstract_mode/include/abstract_mode/abstract_mode.h
+++ Task2/libs/abstract_mode/include/abstract_mode/abstract_mode.h
@@ -1,53 +1,37 @@
 #ifndef TASK2_ABSTRACT_MODE_H
 #define TASK2_ABSTRACT_MODE_H
 
-#include <dlfcn.h>
 #include <iostream>
 #include <string>
 #include <vector>
 
+#include "loaded_strategy.h"
 #include "matrix.h"
-#include "strategy_interface.h"
-
-class LoadedStrategy
-{
-public:
-    std::string name_;
-    Strategy * object_ptr_;
-
-    LoadedStrategy(std::string strategy_name,
-                   std::string lib_dir,
-                   std::string config_dir,
-                   Matrix<int, 8, 3> matrix);
-    ~LoadedStrategy();
-
-private:
-    void * handle_;
-};
-
 
 class Mode
 {
 public:
-    void Init(std::vector<std::string> strategies_names,
+    void Init(std::vector<std::string> strategy_names,
          int steps,
          std::string lib_dir,
          std::string config_dir,
-         Matrix<int, 8, 3> matrix);
+         Matrix matrix);
+    void Clear();
     virtual void Launch() = 0;
     virtual ~Mode();
 
 protected:
     std::vector<LoadedStrategy> strategies_;
     int steps_;
-    Matrix<int, 8, 3> matrix_;
+    Matrix matrix_;
     std::string config_dir_;
-    std::vector<int> score_;
+    std::vector<int> scores_;
 
-    int GetMatrixRowNum(Trio<Choice> choices);
-    virtual Trio<Choice> GetVotingResults(Trio<int> strategy_nums = {0, 1, 2});
-    virtual void UpdateData(Trio<Choice> voting_result,
-                            Trio<int> strategy_nums = {0, 1, 2});
+    Trio<Choice> GetVotingResults(Trio<int> strategy_nums = {0, 1, 2});
+    void UpdateStrategies(Trio<Choice> voting_result,
+                          Trio<int> strategy_nums = {0, 1, 2});
+    void UpdateScores(Trio<Choice> voting_result,
+                      Trio<int> strategy_nums = {0, 1, 2});
 };
 
 #endif //TASK2_ABSTRACT_MODE_H
