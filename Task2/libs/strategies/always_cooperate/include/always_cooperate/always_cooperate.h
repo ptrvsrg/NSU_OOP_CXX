600e8b3a1cfe1f65f43f6eb578e8cd04ff98a1e6
ptrvsrg <s.petrov1@g.nsu.ru>
2022-10-24 21:29:36 +0700

    Move strategy modules

---
+++ Task2/libs/strategies/always_cooperate/include/always_cooperate/always_cooperate.h
@@ -0,0 +1,14 @@
+#ifndef TASK2_ALWAYS_COOPERATE_H
+#define TASK2_ALWAYS_COOPERATE_H
+
+#include "strategy_creator.h"
+#include "strategy_interface.h"
+
+class AlwaysCooperateStrategy : public Strategy
+{
+public:
+    AlwaysCooperateStrategy() = default;
+    Choice vote() override;
+};
+
+#endif // TASK2_ALWAYS_COOPERATE_H
---
+++ Task2/libs/strategies/tit_for_tat/include/tit_for_tat/tit_for_tat.h
@@ -0,0 +1,19 @@
+#ifndef TASK2_TIT_FOR_TAT_H
+#define TASK2_TIT_FOR_TAT_H
+
+#include <vector>
+
+#include "strategy_interface.h"
+
+class TitForTatStrategy : public Strategy
+{
+public:
+    TitForTatStrategy() = default;
+    Choice vote() override;
+    void update(Choice choice1,
+                Choice choice2) override;
+private:
+    Choice choice_ = C;
+};
+
+#endif // TASK2_TIT_FOR_TAT_H
