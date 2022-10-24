a02f3c239298d5d6fe56eb852c4fe96601c90407
ptrvsrg <s.petrov1@g.nsu.ru>
2022-10-24 21:41:32 +0700

    Delete lib_dir option, extra tests

--- Task2/libs/game/test/tests_game.cpp
+++ Task2/libs/game/test/tests_game.cpp
@@ -6,7 +6,6 @@ struct Options
     std::vector<std::string> strategy_name_;
     std::string mode_;
     int steps_ = -1;
-    std::string lib_dir_;
     std::string config_dir_;
     std::string matrix_file_;
     std::string error_message_;
@@ -14,14 +13,12 @@ struct Options
     Options(std::vector<std::string> strategy_name,
             std::string mode,
             int steps,
-            std::string lib_dir,
             std::string config_dir,
             std::string matrix_file,
             std::string error_message)
        : strategy_name_(strategy_name),
         mode_(mode),
         steps_(steps),
-        lib_dir_(lib_dir),
         config_dir_(config_dir),
         matrix_file_(matrix_file),
         error_message_(error_message) {}
@@ -39,36 +36,7 @@ INSTANTIATE_TEST_SUITE_P
                     20,
                     "",
                     "",
-                    "",
-                    "Not enough strategies"),
-            Options({"strategy1", "strategy2", "strategy3"},
-                    "career",
-                    20,
-                    "",
-                    "",
-                    "",
-                    "Wrong modes"),
-            Options({"strategy1", "strategy2", "strategy3"},
-                    "detailed",
-                    20,
-                    "/home/",
-                    "/home/",
-                    "/home/acer/NSU_OOP_CXX/Task2/tests/wrong_matrix0.txt",
-                    "/home/acer/NSU_OOP_CXX/Task2/tests/wrong_matrix0.txt : File opening error"),
-            Options({"strategy1", "strategy2", "strategy3"},
-                    "detailed",
-                    20,
-                    "/home/",
-                    "/home/",
-                    "/home/acer/NSU_OOP_CXX/Task2/tests/wrong_matrix1.txt",
-                    "/home/acer/NSU_OOP_CXX/Task2/tests/wrong_matrix1.txt : Wrong matrix"),
-            Options({"strategy1", "strategy2", "strategy3"},
-                    "detailed",
-                    20,
-                    "/home/",
-                    "/home/",
-                    "/home/acer/NSU_OOP_CXX/Task2/tests/wrong_matrix2.txt",
-                    "/home/acer/NSU_OOP_CXX/Task2/tests/wrong_matrix2.txt : Wrong matrix")
+                    "Not enough strategies")
         )
 );
 
@@ -82,7 +50,6 @@ TEST_P(GameTest, launch_exceptions)
         game.Launch(params.strategy_name_,
                     params.mode_,
                     params.steps_,
-                    params.lib_dir_,
                     params.config_dir_,
                     params.matrix_file_);
     }
