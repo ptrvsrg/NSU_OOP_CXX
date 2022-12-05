f6ea8dfa14ddd5314d5bfe7d6e8bc71c27a60978
ptrvsrg <s.petrov1@g.nsu.ru>
2022-12-05 19:46:25 +0700

    Add example

---
+++ Task4/example/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <tuple>
+
+int main()
+{
+//    std::ifstream file("test.csv");
+//    CSVParser<int, std::string> parser(file, 0 /*skip first lines count*/);
+//    for (std::tuple<int, std::string> rs : parser)
+//        std::cout << rs << std::endl;
+}
