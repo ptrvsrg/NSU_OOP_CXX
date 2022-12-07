f254f1d291fe998c00e9e5f6155830767eff4b23
ptrvsrg <s.petrov1@g.nsu.ru>
2022-12-07 21:10:20 +0700

    Add case with empty tuple

--- Task4/lib/tuple_utility/test/tests_tuple_utility.cpp
+++ Task4/lib/tuple_utility/test/tests_tuple_utility.cpp
@@ -1,40 +1,32 @@
 #include <gtest/gtest.h>
-#include <strstream>
-#include <tuple>
+#include <sstream>
 #include "tuple_utility.h"
 
 TEST(test_tuple_utility,
      check_print_tuple)
 {
-    auto tuple = std::make_tuple(1,
-                                          "dog",
-                                          3.14);
-
     std::stringstream tuple_out;
-    std::stringstream check;
+    std::tuple<> tuple_1;
+    auto tuple_2 = std::make_tuple(1, "dog", 3.14);
 
-    tuple_out << tuple;
-    check << std::get<0>(tuple) << ", "
-          << std::get<1>(tuple) << ", "
-          << std::get<2>(tuple);
+    tuple_out << tuple_1;
+    EXPECT_TRUE(tuple_out.str().empty());
 
-    EXPECT_EQ(tuple_out.str(),
-              check.str());
+    tuple_out << tuple_2;
+    EXPECT_EQ(tuple_out.str(), "1, dog, 3.14");
 }
 
 TEST(tests_tuple,
      check_make_tuple)
 {
-    auto tuple = MakeTuple<int,
-                                    std::string,
-                                    double>({"1",
-                                             "dog",
-                                             "3.14"});
-    auto check = std::make_tuple(1,
-                                          "dog",
-                                          3.14);
-    EXPECT_EQ(tuple,
-              check);
+    auto tuple_1 = MakeTuple<int, std::string, double>({"1", "dog", "3.14"});
+    auto check_1 = std::make_tuple(1, "dog", 3.14);
+    EXPECT_EQ(tuple_1, check_1);
+
+    auto tuple = MakeTuple<>({});
+    auto check = std::tuple<>();
+    EXPECT_EQ(tuple, check);
+
 }
 
 int main(int argc,
