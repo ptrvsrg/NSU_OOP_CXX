71ce7cf5c31aabd0cb9050d801ea7babe3334001
ptrvsrg <s.petrov1@g.nsu.ru>
2022-12-07 21:09:57 +0700

    Add foreach for printing and making of tuple

--- Task4/lib/tuple_utility/include/tuple_utility.h
+++ Task4/lib/tuple_utility/include/tuple_utility.h
@@ -1,106 +1,127 @@
 #ifndef TASK4_TUPLE_UTILITY_H
 #define TASK4_TUPLE_UTILITY_H
 
+#include <memory>
 #include <ostream>
 #include <sstream>
 #include <tuple>
 
 namespace
 {
-    template<typename CharT,
-             typename Traits,
-             size_t First,
-             size_t Last,
-             typename... Types>
-    struct TuplePrinter
+    template <size_t First, size_t Last, size_t Size, typename Callback, typename... Types>
+    class Iteration
     {
-        static void print(std::basic_ostream<CharT, Traits> & out,
-                          const std::tuple<Types...> & value)
+    public:
+        // for printing
+        static void Next(Callback callback, std::tuple<Types...> tuple)
         {
-            out << std::get<First>(value) << ", ";
-            TuplePrinter<CharT,
-                         Traits,
-                         First + 1,
-                         Last,
-                         Types...>::print(out,
-                                          value);
+            callback(std::get<First>(tuple), false);
+            Iteration<First + 1, Last, Size, Callback, Types...>::Next(callback, tuple);
+        }
+
+        // for making
+        static void Next(Callback callback, std::unique_ptr<std::tuple<Types...>> & tuple_ptr)
+        {
+            callback(std::get<First>(*tuple_ptr), false);
+            Iteration<First + 1, Last, Size, Callback, Types...>::Next(callback, tuple_ptr);
         }
     };
 
-    template<typename CharT,
-             typename Traits,
-             size_t Last,
-             typename... Types>
-    struct TuplePrinter<CharT,
-                         Traits,
-                         Last,
-                         Last,
-                         Types...>
+    // Last iteration
+    template <size_t Last, size_t Size, typename Callback, typename... Types>
+    class Iteration<Last, Last, Size, Callback, Types...>
     {
-        static void print(std::basic_ostream<CharT, Traits> & out,
-                          const std::tuple<Types...> & value)
+    public:
+        // for printing
+        static void Next(Callback callback, std::tuple<Types...> tuple)
         {
-            out << std::get<Last>(value);
+            callback(std::get<Last>(tuple), true);
+        }
+
+        // for making
+        static void Next(Callback callback, std::unique_ptr<std::tuple<Types...>> & tuple_ptr)
+        {
+            callback(std::get<Last>(*tuple_ptr), true);
         }
     };
 
-    template<size_t First,
-        size_t Last,
-        typename... Types>
-    struct TupleMaker
+    // empty tuple
+    template <size_t First, size_t Last, typename Callback, typename... Types>
+    class Iteration<First, Last, 0, Callback, Types...>
     {
-        static void make(std::vector<std::string> & params,
-                         std::tuple<Types...> & tuple)
-        {
-            std::istringstream iss(params[0]);
-            iss >> std::get<First>(tuple);
-            params.erase(params.cbegin());
-            TupleMaker<First + 1,
-                       Last,
-                       Types...>::make(params,
-                                       tuple);
-        }
+    public:
+        // for printing
+        static void Next(Callback callback, std::tuple<Types...> tuple) {}
+
+        // for making
+        static void Next(Callback callback, std::unique_ptr<std::tuple<Types...>> & tuple_ptr) {}
     };
 
-    template<size_t Last,
-             typename... Types>
-    struct TupleMaker<Last,
-                      Last,
-                      Types...>
+    // for printing
+    template <typename Callback, typename... Types>
+    void ForEach(Callback callback, std::tuple<Types...> tuple)
     {
-        static void make(std::vector<std::string> & params,
-                         std::tuple<Types...> & tuple)
+        Iteration<0, sizeof...(Types) - 1, sizeof...(Types), Callback, Types...>::Next(callback, tuple);
+    }
+
+    // for making
+    template <typename Callback, typename... Types>
+    void ForEach(Callback callback, std::unique_ptr<std::tuple<Types...>> & tuple_ptr)
+    {
+        Iteration<0, sizeof...(Types) - 1, sizeof...(Types), Callback, Types...>::Next(callback, tuple_ptr);
+    }
+
+    template<typename CharT, typename Traits>
+    class PrintCallback
+    {
+    public:
+        explicit PrintCallback(std::basic_ostream<CharT, Traits> & os)
+            :   m_os(os) {}
+
+        template<typename Type>
+        void operator()(Type value, bool is_last)
         {
-            std::istringstream iss(params[0]);
-            iss >> std::get<Last>(tuple);
+            m_os << value;
+            if (!is_last)
+                m_os << ", ";
         }
+    private:
+        std::basic_ostream<CharT, Traits> & m_os;
+    };
+
+    class MakeCallback
+    {
+    public:
+        explicit MakeCallback(std::vector<std::string> params)
+            :   m_params(std::move(params)) {}
+
+        template<typename Type>
+        void operator()(Type & value, bool is_last)
+        {
+            std::istringstream iss(m_params[0]);
+            iss >> value;
+            m_params.erase(m_params.cbegin());
+        }
+    private:
+        std::vector<std::string> m_params;
     };
 }
 
-template<typename CharT,
-         typename Traits,
-         typename... Types>
-std::basic_ostream<CharT, Traits> & operator<<(std::basic_ostream<CharT, Traits> & os,
-                                               const std::tuple<Types...> & tuple)
+template<typename CharT, typename Traits, typename... Types>
+auto & operator<<(std::basic_ostream<CharT, Traits> & os, const std::tuple<Types...> & tuple)
 {
-    TuplePrinter<CharT,
-                  Traits,
-                  0,
-                  sizeof...(Types) - 1,
-                  Types...>::print(os,
-                                   tuple);
+    PrintCallback<CharT, Traits> print_callback(os);
+    ForEach(print_callback, tuple);
     return os;
 }
 
 template<typename... Types>
-std::tuple<Types...> MakeTuple(std::vector<std::string> params)
+std::tuple<Types...> MakeTuple(const std::vector<std::string> & params)
 {
-    std::tuple<Types...> tuple;
-    TupleMaker<0,
-               sizeof...(Types) - 1,
-               Types...>::make(params,
-                               tuple);
-    return tuple;
+    auto tuple_ptr = std::make_unique<std::tuple<Types...>>();
+    MakeCallback make_callback(params);
+    ForEach(make_callback, tuple_ptr);
+    return *tuple_ptr;
 }
 
 #endif //TASK4_TUPLE_UTILITY_H
