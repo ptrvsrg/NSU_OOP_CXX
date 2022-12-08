90645cde8761fb2a51af177176339ddb883699bf
ptrvsrg <s.petrov1@g.nsu.ru>
2022-12-08 16:29:47 +0700

    Configure the launch of the example

--- Task4/example/main.cpp
+++ Task4/example/main.cpp
@@ -1,10 +1,23 @@
+#include <fstream>
 #include <iostream>
 #include <tuple>
+#include "CSV_parser.h"
 
 int main()
 {
-//    std::ifstream file("test.csv");
-//    CSVParser<int, std::string> parser(file, 0 /*skip first lines count*/);
-//    for (std::tuple<int, std::string> rs : parser)
-//        std::cout << rs << std::endl;
+    std::ifstream file("test.csv");
+
+    try
+    {
+        CSVParser<std::string, int> parser(file, ",", "\n", 1);
+        for (const auto & rs: parser)
+            std::cout << rs << std::endl;
+    }
+    catch (const std::exception & ex)
+    {
+        std::cerr << ex.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
