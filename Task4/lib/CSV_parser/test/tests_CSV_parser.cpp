de23a248204a416adcbd89b9b80764f554a452b7
ptrvsrg <s.petrov1@g.nsu.ru>
2022-12-09 15:15:52 +0700

    Add testing EscapecCharacters; Refactoring

--- Task4/lib/CSV_parser/test/tests_CSV_parser.cpp
+++ Task4/lib/CSV_parser/test/tests_CSV_parser.cpp
@@ -1,29 +1,6 @@
 #include <gtest/gtest.h>
 #include "CSV_parser.h"
 
-template<typename Type>
-class CSVParserTests : public ::testing::Test {};
-TYPED_TEST_SUITE_P(CSVParserTests);
-TYPED_TEST_P(CSVParserTests, check_exception)
-{
-    std::ifstream ifs("files/correct.csv");
-    TypeParam csv_parser(ifs);
-    EXPECT_THROW
-    (
-        {
-            for (const auto & tuple : csv_parser)
-                std::cout << tuple << std::endl;
-        },
-        CSVException
-    );
-}
-REGISTER_TYPED_TEST_SUITE_P(CSVParserTests, check_exception);
-using Types = testing::Types<CSVParser<int, std::string, double>,
-                             CSVParser<int, std::string, double, char, size_t>,
-                             CSVParser<>,
-                             CSVParser<int, size_t, double, char>>;
-INSTANTIATE_TYPED_TEST_SUITE_P(test_CSV_parser, CSVParserTests, Types);
-
 TEST(test_CSV_parser, file_not_open)
 {
     std::ifstream ifs("files/non_exist.csv");
@@ -36,10 +13,75 @@ TEST(test_CSV_parser, file_not_open)
     );
 }
 
+TEST(test_CSV_parser, escape_characters)
+{
+    // File content: "1;\a\n\r\t\v\'\?\\\"\a\n\r\t\v\'\?\\;0.01;a:2;\a\n\r\t\v\'\?\\\"\a\n\r\t\v\'\?\\\";0.01;a"
+    std::ifstream ifs("files/escape.txt");
+    CSVParser<int, std::string, double, char> csv_parser(ifs, 1, ';', ':');
+    for (const auto & tuple : csv_parser)
+        std::cout << tuple << std::endl;
+}
+
+TEST(test_CSV_parser, escape_characters_exception)
+{
+    std::ifstream ifs("files/escape.txt");
+    CSVParser<int, std::string, double, char> csv_parser(ifs, 0, ';', ':');
+    EXPECT_THROW
+    (
+        {
+            for (const auto & tuple : csv_parser)
+                std::cout << tuple << std::endl;
+        },
+        EscapeCharactersException
+    );
+}
+
+TEST(test_CSV_parser, few_columns)
+{
+    std::ifstream ifs("files/correct.csv");
+    CSVParser<int, std::string, double> csv_parser(ifs);
+    EXPECT_THROW
+    (
+        {
+            for (const auto & tuple : csv_parser)
+                std::cout << tuple << std::endl;
+        },
+        ManyColumnsException
+    );
+}
+
+TEST(test_CSV_parser, many_columns)
+{
+    std::ifstream ifs("files/correct.csv");
+    CSVParser<int, std::string, double, char, size_t> csv_parser(ifs);
+    EXPECT_THROW
+    (
+        {
+            for (const auto & tuple : csv_parser)
+                std::cout << tuple << std::endl;
+        },
+        FewColumnsException
+    );
+}
+
+TEST(test_CSV_parser, type_mismatch)
+{
+    std::ifstream ifs("files/correct.csv");
+    CSVParser<int, size_t, double, char> csv_parser(ifs);
+    EXPECT_THROW
+    (
+        {
+            for (const auto & tuple : csv_parser)
+                std::cout << tuple << std::endl;
+        },
+        TypeMismatchException
+    );
+}
+
 TEST(test_CSV_parser, correct_print)
 {
     std::ifstream ifs("files/correct.csv");
-    CSVParser<int, std::string, double, char> csv_parser(ifs, 1, ",", "\n");
+    CSVParser<int, std::string, double, char> csv_parser(ifs, 1);
     for (const auto & tuple : csv_parser)
         std::cout << tuple << std::endl;
 }
