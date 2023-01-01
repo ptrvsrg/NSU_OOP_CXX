1b2782ea446c6d9bd574e12ce9fcfd4c50a10ea8
ptrvsrg <s.petrov1@g.nsu.ru>
2023-01-01 22:17:48 +0700

    Update CSV parser test

--- Task4/lib/CSV_parser/test/tests_CSV_parser.cpp
+++ Task4/lib/CSV_parser/test/tests_CSV_parser.cpp
@@ -28,55 +28,63 @@ TEST(test_CSV_parser, escape_characters_exception)
     CSVParser<std::string, int> csv_parser(ifs, 0, ';', ':', '!');
     EXPECT_THROW
     (
+        {
+            for (const auto & tuple : csv_parser)
+                std::cout << tuple << std::endl;
+        },
+        EscapeSymbolsCountException
+    );
+}
+
+template<typename T>
+struct CSVParserTest : public ::testing::Test {};
+TYPED_TEST_SUITE_P(CSVParserTest);
+
+TYPED_TEST_P(CSVParserTest, check_exception)
+{
+    using CSVParserType = std::tuple_element_t<0, TypeParam>;
+
+    std::ifstream ifs("files/correct.csv");
+
+    if (std::tuple_size_v<TypeParam> == 2)
     {
-            for (const auto & tuple : csv_parser)
-                std::cout << tuple << std::endl;
-        },
-    EscapeSymbolsCountException
-    );
+        using ExceptionType = std::tuple_element_t<1, TypeParam>;
+        CSVParserType csv_parser(ifs);
+        EXPECT_THROW
+        (
+            {
+                for (const auto & tuple: csv_parser)
+                    std::cout << tuple << std::endl;
+            },
+            ExceptionType
+        );
+    }
+    else
+    {
+        EXPECT_NO_THROW
+        (
+            {
+                CSVParserType csv_parser(ifs, 1);
+
+                std::ostringstream print;
+                for (const auto & tuple: csv_parser)
+                    print << tuple << std::endl;
+
+                std::ostringstream print_again;
+                for (const auto & tuple: csv_parser)
+                    print_again << tuple << std::endl;
+
+                EXPECT_EQ(print.str(), print_again.str());
+            }
+        );
+    }
 }
 
-TEST(test_CSV_parser, few_columns)
-{
-    std::ifstream ifs("files/correct.csv");
-    CSVParser<int, std::string, double> csv_parser(ifs);
-    EXPECT_THROW
-    (
-        {
-            for (const auto & tuple : csv_parser)
-                std::cout << tuple << std::endl;
-        },
-        ManyColumnsException
-    );
-}
-
-TEST(test_CSV_parser, many_columns)
-{
-    std::ifstream ifs("files/correct.csv");
-    CSVParser<int, std::string, double, char, size_t> csv_parser(ifs);
-    EXPECT_THROW
-    (
-        {
-            for (const auto & tuple : csv_parser)
-                std::cout << tuple << std::endl;
-        },
-        FewColumnsException
-    );
-}
-
-TEST(test_CSV_parser, type_mismatch)
-{
-    std::ifstream ifs("files/correct.csv");
-    CSVParser<int, size_t, double, char> csv_parser(ifs);
-    EXPECT_THROW
-    (
-        {
-            for (const auto & tuple : csv_parser)
-                std::cout << tuple << std::endl;
-        },
-        TypeMismatchException
-    );
-}
+REGISTER_TYPED_TEST_SUITE_P(CSVParserTest, check_exception);
+using Types = testing::Types<std::tuple<CSVParser<int, std::string, double>, ManyColumnsException>,
+                             std::tuple<CSVParser<int, std::string, double, char, size_t>, FewColumnsException>,
+                             std::tuple<CSVParser<int, size_t, double, char>, TypeMismatchException>>;
+INSTANTIATE_TYPED_TEST_SUITE_P(TestPrefix, CSVParserTest, Types);
 
 TEST(test_CSV_parser, correct_print)
 {
